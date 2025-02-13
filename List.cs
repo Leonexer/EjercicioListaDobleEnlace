603b7aab349755fd8df64f58ea6df2ab8690bc37
aleja <aleja@LAPTOP-02I4B473>
2025-02-12 22:26:10 -0600

    Agregar archivos de proyecto.

---
+++ List.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioListas
+{
+    internal class List
+    {
+        private Nodo Head;
+        private Nodo Tail;
+        private int Counter; //Para el ciclo, acuerdate
+
+        public bool ListIsEmpty()
+        {
+            return Head == null || Tail == null;
+        }
+        public void Prepend(Object Dato)
+        {
+            if (ListIsEmpty())
+            {
+                Head = Tail = new Nodo(Dato);
+                Counter++;
+            }
+            else
+            {
+                Head = Head.prev = new Nodo(Dato, Tail, Head);
+                Counter++;
+            }
+        }
+        public void Append(Object Dato)
+        {
+            if (ListIsEmpty())
+            {
+                Tail = Head = new Nodo(Dato);
+                Counter++;
+            }
+            else
+            {
+                Tail = Tail.next = new Nodo(Dato, Tail, Head);
+                Counter++;
+            }
+        }
+
+        public void ShowList()
+        {
+            Nodo Actual = Head;
+            for (int i = 0; i < Counter; i++)
+            {
+                Console.WriteLine(Actual.Dato);
+                Actual = Actual.next;
+            }
+
+        }
+    }
+}
---
+++ Nodos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioListas
+{
+    internal class Nodo
+    {
+        public object dato;
+        public Nodo next;
+        public Nodo prev;
+
+        public Nodo(object dato, Nodo prev, Nodo next)
+        {
+            this.prev = prev;
+            this.dato = dato;
+            this.next = next;
+        }
+
+        public Nodo(object dato, Nodo prev)
+        {
+            this.dato = dato;
+            this.prev = prev;
+        }
+
+        public Nodo(object dato)
+        {
+            this.dato = dato;
+            this.next = null;
+            this.prev = null;
+        }
+
+        public Nodo()
+        {
+            this.dato = "";
+            this.next = null;
+            this.prev = null;
+        }
+
+        public object Dato
+        {
+            get { return this.dato; }
+            set { this.dato = value; }
+
+        }
+
+        public Nodo Next
+        {
+            get { return this.Next; }
+            set { this.Next = value; }
+        }
+        public Nodo Anterior
+        {
+            get { return this.prev; }
+            set { this.prev = value; }
+        }
+    }
+}
+
+
